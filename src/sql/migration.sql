67215fbc61dd44653c94c91c33cf212ca9a7ce8f
D15G <daavidgtr@outlook.com>
2019-10-25 15:00:56 +0200

    -

---
+++ src/sql/migration.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (fullname, address, postcode, city, uid) VALUES ('David Gataric', 'Ennetbürgerstrasse 4', '6374', 'Buochs', 'rsVShB0etMh5YPLJ9DoJBcZRJjv1');
+
+INSERT INTO product (name, price, color, image, brand, type) VALUES ('Nike Air Force', 175.95, 'weiss', 'https://snipes.scene7.com/is/image/snipes/1126425_P?$s_product_detail$', 'nike', 'Schuhe');
