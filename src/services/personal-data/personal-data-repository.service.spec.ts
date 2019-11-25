b4d47ece7c5e2e616a96293091dbfa5904bc3347
D15G <daavidgtr@outlook.com>
2019-11-25 12:54:02 +0100

    added personalDataController tests and travis config

---
+++ src/services/personal-data/personal-data-repository.service.spec.ts
@@ -0,0 +1,37 @@
+import {PersonalDataController} from '../../controllers/personal-data/personal-data.controller';
+import {PersonalDataRepository} from './personal-data-repository.service';
+
+describe('PersonalDataController', () => {
+    let personalDataController: PersonalDataController;
+    let personalDataService: PersonalDataRepository;
+
+    beforeEach(async () => {
+        personalDataService = new PersonalDataRepository();
+        personalDataController = new PersonalDataController(personalDataService);
+    });
+
+    describe('testGetPersonalDataByUIDWhenUIDValid', () => {
+        it('should return personalData', async () => {
+            const expected = {
+                id: 1,
+                fullname: 'Donald Duck',
+                address: 'Some Address',
+                postcode: '4246',
+                city: 'Some City',
+                uid: 'ValidUid',
+            };
+            jest.spyOn(personalDataService, 'getPersonalDataByUID').mockImplementation(() => Promise.resolve(expected));
+
+            expect(await personalDataController.getPersonalDataByUID('ValidUid')).toBe(expected);
+        });
+    });
+
+    describe('testGetPersonalDataByUIDWhenUIDInvalid', () => {
+        it('should return empty personalData', async () => {
+            const expected = {};
+            jest.spyOn(personalDataService, 'getPersonalDataByUID').mockImplementation(() => Promise.resolve(expected));
+
+            expect(await personalDataController.getPersonalDataByUID('InvalidUid')).toBe(expected);
+        });
+    });
+});
